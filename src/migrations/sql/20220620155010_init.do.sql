01182eb4024199bfaffd89fb9ee3ec81fbd2a7ab
peterfulop <peterfulop@codingsans.com>
2022-06-20 18:09:39 +0200

    feat(*): initial db state updated;

--- src/migrations/sql/20220620155010_init.do.sql
+++ src/migrations/sql/20220620155010_init.do.sql
@@ -5,7 +5,23 @@ CREATE TABLE products (
   "quantity" int NOT NULL,
   "image" text NOT NULL,
   "price" float NOT NULL,
-  "onSale" boolean NOT NULL,
+  "onSale"  boolean DEFAULT true NOT NULL,
   "category" text NOT NULL,
   CONSTRAINT "productsPkey" PRIMARY KEY ("id")
+);
+
+CREATE TABLE categories (
+  "id" uuid NOT NULL,
+  "name" text NOT NULL,
+  CONSTRAINT "categoriesPkey" PRIMARY KEY ("id")
+);
+
+CREATE TABLE reviews (
+  "id" uuid NOT NULL,
+  "date" TIMESTAMPTZ NOT NULL DEFAULT (now()),
+  "title" text NOT NULL,
+  "comment" text NOT NULL,
+  "rating" int NOT NULL,
+  "productId" text NULL,
+  CONSTRAINT "reviewsPkey" PRIMARY KEY ("id")
 );
--- src/migrations/sql/20220620155010_init.undo.sql
+++ src/migrations/sql/20220620155010_init.undo.sql
@@ -1,1 +1,3 @@
 DROP TABLE products CASCADE;
+DROP TABLE categories CASCADE;
+DROP TABLE reviews CASCADE;
